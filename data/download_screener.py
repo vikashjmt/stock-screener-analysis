16ffa39c5d77fe375fbd6791fc9efd0b824509ca
Vikash Kumar <vickyjmt0595@gmail.com>
2025-02-22 21:27:14 +0530

    Prepare to run the project using Github Action

--- data/download_screener.py
+++ data/download_screener.py
@@ -2,41 +2,49 @@ import json
 
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from icecream import ic
+# from icecream import ic
 from pathlib import Path
 from shutil import move
 from datetime import datetime
 
+options = Options()
+options.add_argument("--headless")  # Run in headless mode
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+
 def download_screener(url):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(6)
     driver.get(url)
     sleep(6)
     dom = driver.find_element(by=By.CSS_SELECTOR, value="a.btn-primary")
-    ic(dom)
     sleep(6)
     dom.click()
     sleep(4)
     driver.quit()
 
+
 def get_latest_download():
     download_folder = Path.home() / 'Downloads'
     # Get all csv files
     files = download_folder.glob('*csv')
-    return max([file for file in files], 
+    return max([file for file in files],
                key=lambda item: item.stat().st_ctime)
+
 
 def get_data(config_file):
     with open(config_file) as fd:
         data = json.load(fd)
     return data
 
+
 if __name__ == "__main__":
     # Map screener url with screener type and its destination folder
     data_dir = Path(__file__).parent
     config_file = f"{data_dir}/data_config.json"
-    ic(config_file)
     # Download screeners csvs
     screener_csvs = list()
     screener_output_file = f"{data_dir}/downloaded_csvs.json"
@@ -47,12 +55,10 @@ if __name__ == "__main__":
             old_screener_data = json.load(fd)
     screener_downloaded = dict()
     data = get_data(config_file)
-    ic(data)
-    # screener_url = "https://chartink.com/screener/high-volume-on-sma-20-crossover"
+    # ic(data)
     for screener in data:
-        # screener_url = "https://chartink.com/screener/stocks-trending-above-10-ema-for-a-month"
-        # destination_folder = f"sample_screener_name/{datetime.today().strftime('%Y.%m.%d')}.csv"
         screener_url = data[screener]['url']
+        print(f'Downloading screener {data[screener]}')
         destination_folder = f"{data_dir}/{data[screener]['folder']}"
         Path(destination_folder).mkdir(parents=True,
                                        exist_ok=True)
@@ -61,7 +67,7 @@ if __name__ == "__main__":
         if not old_screener_data.get(destination_file):
             download_screener(screener_url)
             latest_file = get_latest_download()
-            fetched_file = move(latest_file,destination_file)
+            fetched_file = move(latest_file, destination_file)
         else:
             fetched_file = destination_file
         screener_csvs.append(fetched_file)
@@ -69,4 +75,8 @@ if __name__ == "__main__":
 
     # Add the downloaded files to the csv
     with open(screener_output_file, 'w') as fd:
-       json.dump(screener_downloaded, fd)
+        json.dump(screener_downloaded, fd)
+
+    # Get Downloaded csv files
+    print('Details of downloaded screenrs can be found '
+          f'in {screener_output_file}')
